c656126d4f613d31d69214fcce126df77aeb9a08
rickylim19 <rickylim19@gmail.com>
2013-03-12 16:52:21 +0100

    fix the matrix init

--- getTransitionMatrix.R
+++ getTransitionMatrix.R
@@ -1,7 +1,8 @@
 x <- c(1,2,1,1,3,4,1,1)
 
 getTransitionMatrix <- function(x){
-  p <- matrix(nrow = length(as.numeric(levels(as.factor(x)))), ncol = 4, 0)
+  n  = length(as.numeric(levels(as.factor(x))))
+  p <- matrix(nrow = n, ncol = n, 0)
   for (t in 1:(length(x) - 1)) p[x[t], x[t + 1]] <- p[x[t], x[t + 1]] + 1
   #normalize the matrix
   for (i in 1:4) p[i, ] <- p[i, ] / sum(p[i, ])
